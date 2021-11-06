30db7293ac59210c95d715fcae606a4de034196a
Andres Vasquez <60708395+avasquez4@users.noreply.github.com>
2021-11-06 04:04:18 -0400

    Added welcome and verify embeds.

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,3 +1,5 @@
+const message = require('./message');
+
 module.exports = (Discord, client, member) => {
 	var channel = client.channels.cache.get('906056492766097421'); //Channel id for verification
 
@@ -5,18 +7,48 @@ module.exports = (Discord, client, member) => {
 
 	member.roles.add('906081086226972675'); //Role id for unverified
 
-	channel.send('Welcome to our server <@' + member.user.id + '>!.');
-	channel.send(
-		'For access to server, please follow the instructions Dmd to you.'
-	);
-	channel.send(
-		'If you have any questions, please use the !help command, or ask an Admin for help.'
-	);
+	//Create and send embed
+	const welcomeEmbed = new Discord.MessageEmbed()
+		.setColor('#304281')
+		.setTitle('Rules')
+		//.setURL('www.csoesu.org')
+		.setDescription('Please read the rules below for access to the server.')
+		.addFields(
+			{ name: 'Rule 1:', value: 'Must be an ESU student.' },
+			{ name: 'Rule 2:', value: 'Must have valid @live.esu.edu email' },
+			{
+				name: 'Rule 3:',
+				value: 'Must be verified (Check DM from Verification Bot)',
+			}
+		)
+		.setFooter(
+			'If you have any questions, please use the !help command, or ask an Admin for help.'
+		);
 
-	member.send(
-		'Begin the verification process with !verify command followed by your real first and last name, and email - all seperated by spaces.'
-	);
-	member.send('You will need a valid @esu.edu email to join the server.');
+	channel.send('Welcome to our server <@' + member.user.id + '>!.');
+	channel.send({ embeds: [welcomeEmbed] });
+
+	const verifyEmbed = new Discord.MessageEmbed()
+		.setColor('#304281')
+		.setTitle('Verification Process')
+		.setDescription('Please follow the steps below for access to the server.')
+		.addFields(
+			{
+				name: 'Step 1:',
+				value:
+					'Begin the verification process with !verify command followed by your real first and last name, and email - all seperated by spaces.',
+			},
+			{
+				name: 'Step 2:',
+				value: 'You will be sent a verification code to the provided email.',
+			},
+			{ name: 'Step 3:', value: 'Reply with the code.' }
+		)
+		.setFooter(
+			'If you have any questions, please use the !help command, or ask an Admin for help.'
+		);
+
+	member.send({ embeds: [verifyEmbed] });
 
 	//save when user was added member.joindate()
 };

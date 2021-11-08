65745a18301d1e6608af0bd8b8ca0f6b70a6c750
Andres Vasquez <60708395+avasquez4@users.noreply.github.com>
2021-11-07 21:22:55 -0500

    Verify command asks user for confirmation

--- commands/verify.js
+++ commands/verify.js
@@ -1,12 +1,13 @@
+const messageReactionAdd = require('../events/client/messageReactionAdd');
+
 module.exports = {
 	name: 'verify',
 	description: 'Verify command',
-	execute(client, message, args, Discord) {
+	async execute(client, message, args, Discord) {
 		if (message.channel.type === 'DM' && !message.author.bot) {
 			console.log('Verify - DM Recived');
 
-			//Create and send embed
-
+			//get User input
 			const email = args.shift();
 			var name = '';
 			args.forEach((word) => {
@@ -20,8 +21,6 @@ module.exports = {
 			var validemail = regexEmail.test(email);
 			var validname = regexname.test(name);
 
-			console.log(validemail + '' + validname);
-
 			if (validemail && validname) {
 				const confirmEmbed = new Discord.MessageEmbed()
 					.setColor('#304281')
@@ -33,30 +32,55 @@ module.exports = {
 						{ name: 'Name:', value: '' + name },
 						{ name: 'Email:', value: '' + email }
 					)
-					.setFooter('Message will expire after 45 seconds.');
+					.setFooter('Message will expire after 30 seconds.');
 
-				message.author.send({ embeds: [confirmEmbed] }).then((embedMessage) => {
-					embedMessage.react('✅').then(() => embedMessage.react('❌'));
-				});
+				let embed = await message.reply({ embeds: [confirmEmbed] });
 
-			} else {
-				message.author.send('Please try the !verify command again.');
-				/**
-				 * Wait 30 seconds till react, else timeout if time exceeded
-				 * else if reponded to react, generate 6-digit code and set server nickname to name
-				 * call email_verification
-				 *
-				 * function email_verification(name, email, memberid, joindate, code)
-				 * Store code, member id, member join date in Database
-				 * Match responded code (must reply with 24 hours of code being sent (use member join date))
-				 * with code in database using message.member.id and memberID in DB
-				 * compare join date with message's date
-				 * if join date is > message's date (delete member from DB(done automatically in DB))
-				 *|| member does not exist in DB (b/c auto deleted), must !verify again
-				 * else if code sent == code in DB && join date < message's date
-				 * member.role == verified
-				 **/
+				await embed.react('✅');
+				await embed.react('❌');
+
+				const filter = (reaction, user) => {
+					return ['✅', '❌'].includes(reaction.emoji.name) && !user.bot;
+				};
+
+				embed
+					.awaitReactions({ filter, max: 1, time: 10000, errors: ['time'] })
+					.then((collected) => {
+						const reaction = collected.first();
+
+						if (reaction.emoji.name === '✅') {
+							embed.delete();
+							message.reply('Information confirmed!');
+						} else {
+							embed.delete();
+							message.reply('Use the !verify command again.');
+						}
+					})
+					.catch((collected) => {
+						embed.delete();
+						message.reply('Message expired.');
+					});
+			} else if (!validemail) {
+				message.reply('Invalid email. Please try the !verify command again.');
+			} else if (!validname) {
+				message.reply('Invalid email. Please try the !verify command again.');
 			}
-		}
+		} else message.reply('That command cannot be used here.');
 	},
 };
+
+/**
+ * Wait 30 seconds till react, else timeout if time exceeded
+ * else if reponded to react, generate 6-digit code and set server nickname to name
+ * call email_verification
+ *
+ * function email_verification(name, email, memberid, joindate, code)
+ * Store code, member id, member join date in Database
+ * Match responded code (must reply with 24 hours of code being sent (use member join date))
+ * with code in database using message.member.id and memberID in DB
+ * compare join date with message's date
+ * if join date is > message's date (delete member from DB(done automatically in DB))
+ *|| member does not exist in DB (b/c auto deleted), must !verify again
+ * else if code sent == code in DB && join date < message's date
+ * member.role == verified
+ **/
